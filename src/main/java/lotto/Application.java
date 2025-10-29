24f2835bbb0cfe558b10ac0f654fe7ee1e70c9f9
김민석 <skyfox1015@gmail.com>
2025-10-29 16:29:55 +0900

    feat(app): 랜덤으로 된 로또 번호 6개 Lotto 리스트에 저장
    - Random 메서드를 이용해서 6가지의 숫자를 획득
    - Lotto로 된 Lotto리스트를 생성 및 저장

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,5 +1,9 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.List;
+
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class Application {
@@ -9,5 +13,13 @@ public class Application {
         String buyAmountInput = readLine();
         int buyAmount = Integer.parseInt(buyAmountInput);
 
+        int lottoCount = buyAmount/1000;
+
+        Lotto[] Lottos = new Lotto[lottoCount];
+
+        for(int i=0;i<lottoCount;i++){
+            List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            Lottos[i] = new Lotto(numbers);
+        }
     }
 }
