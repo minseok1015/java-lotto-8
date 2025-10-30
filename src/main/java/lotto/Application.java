e2ae1a503e54f5a38a4d81a1238a57017fdea882
김민석 <skyfox1015@gmail.com>
2025-10-30 14:50:37 +0900

    feat(app): 수익률 계산 메서드 구현
    - Map에 저장된 등수별 당첨을 이용하여 수익률을 계산
    - 수익률을 소숫점 둘째자리에서 반올림한다

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -43,6 +43,26 @@ public class Application {
         String bonusInput = readLine();
         int bonus = Integer.parseInt(bonusInput);
 
+        HashMap<Integer,Integer> winningCountMap = new HashMap<>();
 
+
+        for(int i=0;i<lottoCount;i++){
+            int rank = lottos[i].calculateRank(winningNumber,bonus);
+            winningCountMap.put(rank,winningCountMap.getOrDefault(rank,0)+1);
+        }
+
+        double profitRate = calculateProfitRate(winningCountMap,buyAmount);
+
+    }
+
+    private static double calculateProfitRate(HashMap<Integer, Integer> winningCountMap, int buyAmount){
+        int totalPrize=0;
+        totalPrize += winningCountMap.getOrDefault(5,0) * 5000;
+        totalPrize += winningCountMap.getOrDefault(4,0) * 50000;
+        totalPrize += winningCountMap.getOrDefault(3,0) * 1500000;
+        totalPrize += winningCountMap.getOrDefault(2,0) * 30000000;
+        totalPrize += winningCountMap.getOrDefault(1,0) * 2000000000;
+
+        return Math.round((double) totalPrize / buyAmount * 100 * 100) / 100.0;
     }
 }
