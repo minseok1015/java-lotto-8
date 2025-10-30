5c4714f26d39ccb54b34935a177a730510812ca0
김민석 <skyfox1015@gmail.com>
2025-10-30 14:46:43 +0900

    feat(lotto): 몇등 당첨인지 확인하는 메서드 구현
    - return은 int로 하며, 그 return값은 등수를 나타냄

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class Lotto {
@@ -23,4 +24,36 @@ public class Lotto {
         numbers.sort(Integer::compareTo);
         return numbers.toString();
     }
+
+    public int calculateRank(HashSet<Integer> winningNumber,int bonus){
+        int count=0;
+        boolean isBonus=false;
+        for (Integer number : numbers) {
+            if(winningNumber.contains(number)){
+                count++;
+            }
+            if(number==bonus){
+                isBonus=true;
+            }
+        }
+
+        if(count==3){
+            return 5;
+        }
+        if(count==4){
+            return 4;
+        }
+        if(count==5&&!isBonus){
+            return 3;
+        }
+        if(count==5){
+            return 2;
+        }
+        if(count==6){
+            return 1;
+        }
+        return 6;
+    }
+
+
 }
