7363f23bde1b1ea4599e77254390167b09d5cc0d
김민석 <skyfox1015@gmail.com>
2025-11-01 23:35:28 +0900

    refactor(app): Application에 OutputView 적용
    - 구매 개수, 로또 목록, 통계 헤더/집계/수익률 출력 로직을 OutputView로 이전

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -2,6 +2,7 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Randoms;
 import view.InputView;
+import view.OutputView;
 
 
 import java.util.HashMap;
@@ -13,6 +14,8 @@ public class Application {
     public static void main(String[] args) {
 
         InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+
         int buyAmount = inputView.readPurchaseAmount();
 
         int lottoCount = buyAmount/1000;
@@ -24,10 +27,8 @@ public class Application {
             lottos[i] = new Lotto(numbers);
         }
 
-        System.out.println(lottoCount+"개를 구매했습니다.");
-        for (int i = 0; i < lottoCount; i++) {
-            System.out.println(lottos[i].toString());
-        }
+        outputView.printPurchaseCount(lottoCount);
+        outputView.printLottos(lottos);
 
         HashSet<Integer> winningNumber = inputView.readWinningNumbers();
         int bonus = inputView.readBonusNumber();
@@ -42,15 +43,9 @@ public class Application {
 
         double profitRate = calculateProfitRate(winningCountMap,buyAmount);
 
-        System.out.println();
-        System.out.println("당첨 통계");
-        System.out.println("---");
-        System.out.println("3개 일치 (5, 000원) - "+winningCountMap.getOrDefault(5,0)+"개");
-        System.out.println("4개 일치 (50,000원) - "+winningCountMap.getOrDefault(4,0)+"개");
-        System.out.println("5개 일치 (1,500,000원) - "+winningCountMap.getOrDefault(3,0)+"개");
-        System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - "+winningCountMap.getOrDefault(2,0)+"개");
-        System.out.println("6개 일치 (2,000,000,000원) - "+winningCountMap.getOrDefault(1,0)+"개");
-        System.out.println("총 수익률은 "+profitRate +"% 입니다");
+        outputView.printStatisticsHeader();
+        outputView.printWinningCounts(winningCountMap);
+        outputView.printProfitRate(profitRate);
 
     }
 
