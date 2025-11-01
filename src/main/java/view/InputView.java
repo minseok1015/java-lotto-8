452f90688f79bdca17c756c8027e64e8697ebac8
김민석 <skyfox1015@gmail.com>
2025-11-01 23:25:39 +0900

    feat(view): InputView 클래스 생성
    - 사용자 입력을 담당하는 InputView 추가
    - 구입 금액, 당첨 번호, 보너스 번호 입력 메서드 복사

---
+++ src/main/java/view/InputView.java
@@ -0,0 +1,33 @@
+package view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.HashSet;
+
+public class InputView {
+
+    public int readPurchaseAmount() {
+        System.out.println("구입금액을 입력해 주세요.");
+        String input = Console.readLine();
+        return Integer.parseInt(input);
+    }
+
+    public HashSet<Integer> readWinningNumbers() {
+        System.out.println();
+        System.out.println("당첨 번호를 입력해 주세요.");
+        String winningNumberInput = Console.readLine();
+        String[] winningNumbersInput = winningNumberInput.split(",");
+        HashSet<Integer> winningNumber = new HashSet<>();
+        for (int i = 0; i < 6; i++) {
+            winningNumber.add(Integer.parseInt(winningNumbersInput[i]));
+        }
+        return winningNumber;
+    }
+
+    public int readBonusNumber() {
+        System.out.println();
+        System.out.println("보너스 번호를 입력해 주세요.");
+        String bonusInput = Console.readLine();
+        return Integer.parseInt(bonusInput);
+    }
+}
