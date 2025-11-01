e94b15c1f33bca78739ac4f8119fa0efe189a8d3
김민석 <skyfox1015@gmail.com>
2025-11-02 01:30:51 +0900

    feat(winning): 생성자 추가

--- src/main/java/lotto/domain/Winning.java
+++ src/main/java/lotto/domain/Winning.java
@@ -1,4 +1,15 @@
 package lotto.domain;
 
+import java.util.HashSet;
+
 public class Winning {
+    private final HashSet<Integer> numbers;
+    private final int bonus;
+
+    public Winning(HashSet<Integer> numbers, int bonus) {
+        this.numbers = numbers;
+        this.bonus = bonus;
+    }
+
+    
 }
