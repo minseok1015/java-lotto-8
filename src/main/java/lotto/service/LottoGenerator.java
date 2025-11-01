06b6062b8f210d93ba1b03536ab39b68cdb99599
김민석 <skyfox1015@gmail.com>
2025-11-02 01:14:36 +0900

    feat(service): 기존의 Lotto생성 로직 LottoGenerator로 위임

--- src/main/java/lotto/service/LottoGenerator.java
+++ src/main/java/lotto/service/LottoGenerator.java
@@ -1,4 +1,24 @@
 package lotto.service;
 
+
+import camp.nextstep.edu.missionutils.Randoms;
+import lotto.Lotto;
+
+import java.util.List;
+
 public class LottoGenerator {
+
+    public Lotto generate() {
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        return new Lotto(numbers);
+    }
+
+    public Lotto[] generateLottos(int count) {
+        Lotto[] lottos = new Lotto[count];
+        for (int i = 0; i < count; i++) {
+            lottos[i] = generate();
+        }
+        return lottos;
+    }
 }
+
