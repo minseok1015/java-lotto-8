77d9c6d91e80704d543f86aed5d45d9101f806f1
김민석 <skyfox1015@gmail.com>
2025-11-02 00:22:20 +0900

    feat(money): 3.1.2 로또 구입 금액이 음수일 경우 에러 처리 추가
    - Money 클래스에서 of로 기본생성자를  생성할때, validate 메서드를 추가해서 검증 처리

--- src/main/java/lotto/domain/Money.java
+++ src/main/java/lotto/domain/Money.java
@@ -6,6 +6,7 @@ public class Money {
     private final int amount;
 
     private Money(int amount) {
+        validate(amount);
         this.amount = amount;
     }
 
@@ -13,7 +14,11 @@ public class Money {
         return new Money(amount);
     }
 
-
+    private void validate(int amount) {
+        if (amount <= 0) {
+            throw new IllegalArgumentException("[ERROR] 금액은 0보다 커야 합니다.");
+        }
+    }
 
     public int calculateLottoCount() {
         return amount / LOTTO_PRICE;
