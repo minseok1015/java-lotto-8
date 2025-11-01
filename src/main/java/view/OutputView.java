89c0e16d560920dcb63756b04a6b3773fdb25666
김민석 <skyfox1015@gmail.com>
2025-11-01 23:31:30 +0900

    feat(view): OutputView 클래스 생성
    - 출력 전용 OutputView 추가
    - 구매 개수, 로또 목록, 통계 헤더/집계/수익률 출력 메서드 정의

---
+++ src/main/java/view/OutputView.java
@@ -0,0 +1,35 @@
+package view;
+
+import lotto.Lotto;
+import java.util.HashMap;
+
+public class OutputView {
+
+    public void printPurchaseCount(int lottoCount) {
+        System.out.println(lottoCount + "개를 구매했습니다.");
+    }
+
+    public void printLottos(Lotto[] lottos) {
+        for (int i = 0; i < lottos.length; i++) {
+            System.out.println(lottos[i].toString());
+        }
+    }
+
+    public void printStatisticsHeader() {
+        System.out.println();
+        System.out.println("당첨 통계");
+        System.out.println("---");
+    }
+
+    public void printWinningCounts(HashMap<Integer, Integer> winningCountMap) {
+        System.out.println("3개 일치 (5, 000원) - " + winningCountMap.getOrDefault(5, 0) + "개");
+        System.out.println("4개 일치 (50,000원) - " + winningCountMap.getOrDefault(4, 0) + "개");
+        System.out.println("5개 일치 (1,500,000원) - " + winningCountMap.getOrDefault(3, 0) + "개");
+        System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - " + winningCountMap.getOrDefault(2, 0) + "개");
+        System.out.println("6개 일치 (2,000,000,000원) - " + winningCountMap.getOrDefault(1, 0) + "개");
+    }
+
+    public void printProfitRate(double profitRate) {
+        System.out.println("총 수익률은 " + profitRate + "% 입니다");
+    }
+}
