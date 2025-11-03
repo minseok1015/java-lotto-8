4b48976fc9703d101283cb3b4ea6715cd8bd0e5c
김민석 <skyfox1015@gmail.com>
2025-11-03 19:00:55 +0900

    refactor(lotto): run 분리(15라인 이하,단일 책임)
    -readMoney/issueLottos/printPurchase/readWinning/judgeAll/printStats로 세분화
    - 동작 변경 없음, 가독성/응집도 향상

--- src/main/java/lotto/service/LottoGameService.java
+++ src/main/java/lotto/service/LottoGameService.java
@@ -2,14 +2,12 @@ package lotto.service;
 
 import lotto.Lotto;
 import lotto.domain.Money;
-import lotto.domain.Rank;
 import lotto.domain.Winning;
 import lotto.util.Retry;
 import lotto.view.InputView;
 import lotto.view.OutputView;
 
 import java.util.List;
-import java.util.Set;
 
 public class LottoGameService {
 
@@ -30,41 +28,49 @@ public class LottoGameService {
     }
 
     public void run() {
-        // 1) 구매 금액 입력/검증
-        Money buyAmount = Retry.readParsed(inputView::readPurchaseAmount, Money::from);
-        int lottoCount = buyAmount.calculateLottoCount();
+        Money money = readMoney();
+        List<Lotto> lottos = issueLottos(money.calculateLottoCount());
+        printPurchase(lottos);
 
-        // 2) 로또 발행
-        List<Lotto> lottos = lottoGenerator.generateLottos(lottoCount);
+        Winning winning = readWinning();
+        LottoResultCounter counter = judgeAll(lottos, winning);
 
-        // 3) 구매 결과 출력
-        outputView.printPurchaseCount(lottoCount);
+        printStats(counter, money.value());
+    }
+
+    private Money readMoney() {
+        return Retry.readParsed(inputView::readPurchaseAmount, Money::from);
+    }
+
+    private List<Lotto> issueLottos(int count) {
+        return lottoGenerator.generateLottos(count);
+    }
+
+    private void printPurchase(List<Lotto> lottos) {
+        outputView.printPurchaseCount(lottos.size());
         outputView.printLottos(lottos);
+    }
 
-        // 4) 당첨/보너스 입력 + 검증
-        Set<Integer> winningNumbers = Retry.readParsed(
-                inputView::readWinningNumbersLine,
-                lotto.util.WinningInputParser::parseWinningNumbers
-        );
+    private Winning readWinning() {
+        var nums = Retry.readParsed(inputView::readWinningNumbersLine,
+                lotto.util.WinningInputParser::parseWinningNumbers);
+        int bonus = Retry.readParsed(inputView::readBonusNumberLine,
+                lotto.util.WinningInputParser::validateBonus);
+        return Winning.of(nums, bonus);
+    }
 
-        int bonus = Retry.readParsed(
-                inputView::readBonusNumberLine,
-                lotto.util.WinningInputParser::validateBonus
-        );
-
-        Winning winning = Winning.of(winningNumbers, bonus);
-
-        // 5) 결과 집계
+    private LottoResultCounter judgeAll(List<Lotto> lottos, Winning winning) {
         LottoResultCounter counter = new LottoResultCounter();
         for (Lotto lotto : lottos) {
-            Rank rank = winning.judge(lotto);
-            counter.add(rank);
+            counter.add(winning.judge(lotto));
         }
+        return counter;
+    }
 
-        // 6) 통계/수익률 출력
-        double profitRate = counter.profitRate(buyAmount.value());
+    private void printStats(LottoResultCounter counter, int buyAmount) {
         outputView.printStatisticsHeader();
         outputView.printWinningCounts(counter.result());
-        outputView.printProfitRate(profitRate);
+        outputView.printProfitRate(counter.profitRate(buyAmount));
     }
+
 }
