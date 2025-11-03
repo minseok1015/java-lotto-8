45c9f8d82aa5114104ca5d75a6bdbf3bc9f3a331
김민석 <skyfox1015@gmail.com>
2025-11-03 16:33:41 +0900

    feat(winning): 3.3.1 보너스 번호가 기존 번호 6개 중 하나랑 겹칠 경우

--- src/main/java/lotto/domain/Winning.java
+++ src/main/java/lotto/domain/Winning.java
@@ -15,6 +15,9 @@ public class Winning {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("당첨 번호는 6개여야 합니다.");
         }
+        if (numbers.contains(bonus)) {
+            throw new IllegalArgumentException("보너스 번호는 당첨 번호와 달라야 합니다.");
+        }
         return new Winning(numbers, bonus);
     }
     
