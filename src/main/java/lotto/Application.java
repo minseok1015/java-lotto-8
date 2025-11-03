5d97d8a0d0e908865ff2a810486b82f840123f95
김민석 <skyfox1015@gmail.com>
2025-11-03 18:52:41 +0900

    refactor(lotto): 게임 흐름을 LottoGameService로 통합
    Application -> LottoGameService로 흐름 이전, 주입형 생성자 추가,
    main은 부트스트랩만 수행. 기능 변화 없음(동일 출력 보장)

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,57 +1,9 @@
 package lotto;
 
-import lotto.domain.Money;
-import lotto.domain.Rank;
-import lotto.domain.Winning;
-import lotto.service.LottoGenerator;
-import lotto.service.LottoResultCounter;
-import lotto.util.Retry;
-import lotto.view.InputView;
-import lotto.view.OutputView;
-import java.util.List;
-import java.util.Set;
-
+import lotto.service.LottoGameService;
 
 public class Application {
     public static void main(String[] args) {
-
-        InputView inputView = new InputView();
-        OutputView outputView = new OutputView();
-
-        Money buyAmount = Retry.readParsed(inputView::readPurchaseAmount, Money::from);
-        int lottoCount = buyAmount.calculateLottoCount();
-
-        LottoGenerator generator = new LottoGenerator();
-        List<Lotto> lottos = generator.generateLottos(lottoCount);
-
-        outputView.printPurchaseCount(lottoCount);
-        outputView.printLottos(lottos);
-
-        Set<Integer> winningNumbers = Retry.readParsed(
-                inputView::readWinningNumbersLine,
-                lotto.util.WinningInputParser::parseWinningNumbers
-        );
-
-        int bonus = Retry.readParsed(
-                inputView::readBonusNumberLine,
-                lotto.util.WinningInputParser::validateBonus
-        );
-
-        Winning winning = Winning.of(winningNumbers, bonus);
-
-        LottoResultCounter counter = new LottoResultCounter();
-
-        for (Lotto lotto : lottos) {
-            Rank rank = winning.judge(lotto);
-            counter.add(rank);
-        }
-
-        double profitRate = counter.profitRate(buyAmount.value());
-
-        outputView.printStatisticsHeader();
-        outputView.printWinningCounts(counter.result());
-        outputView.printProfitRate(profitRate);
-
+        new LottoGameService().run();
     }
-
 }
--- src/main/java/lotto/service/LottoGameService.java
+++ src/main/java/lotto/service/LottoGameService.java
@@ -1,4 +1,70 @@
 package lotto.service;
 
+import lotto.Lotto;
+import lotto.domain.Money;
+import lotto.domain.Rank;
+import lotto.domain.Winning;
+import lotto.util.Retry;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
+import java.util.List;
+import java.util.Set;
+
 public class LottoGameService {
+
+    private final InputView inputView;
+    private final OutputView outputView;
+    private final LottoGenerator lottoGenerator;
+
+    // 기본 생성자
+    public LottoGameService() {
+        this(new InputView(), new OutputView(), new LottoGenerator());
+    }
+
+    // 주입 가능한 생성자 (테스트)
+    public LottoGameService(InputView inputView, OutputView outputView, LottoGenerator lottoGenerator) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+        this.lottoGenerator = lottoGenerator;
+    }
+
+    public void run() {
+        // 1) 구매 금액 입력/검증
+        Money buyAmount = Retry.readParsed(inputView::readPurchaseAmount, Money::from);
+        int lottoCount = buyAmount.calculateLottoCount();
+
+        // 2) 로또 발행
+        List<Lotto> lottos = lottoGenerator.generateLottos(lottoCount);
+
+        // 3) 구매 결과 출력
+        outputView.printPurchaseCount(lottoCount);
+        outputView.printLottos(lottos);
+
+        // 4) 당첨/보너스 입력 + 검증
+        Set<Integer> winningNumbers = Retry.readParsed(
+                inputView::readWinningNumbersLine,
+                lotto.util.WinningInputParser::parseWinningNumbers
+        );
+
+        int bonus = Retry.readParsed(
+                inputView::readBonusNumberLine,
+                lotto.util.WinningInputParser::validateBonus
+        );
+
+        Winning winning = Winning.of(winningNumbers, bonus);
+
+        // 5) 결과 집계
+        LottoResultCounter counter = new LottoResultCounter();
+        for (Lotto lotto : lottos) {
+            Rank rank = winning.judge(lotto);
+            counter.add(rank);
+        }
+
+        // 6) 통계/수익률 출력
+        double profitRate = counter.profitRate(buyAmount.value());
+        outputView.printStatisticsHeader();
+        outputView.printWinningCounts(counter.result());
+        outputView.printProfitRate(profitRate);
+    }
 }
