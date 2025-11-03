a1f8a01da3979e7e34eff049d7fadb455b3511ce
김민석 <skyfox1015@gmail.com>
2025-11-03 19:35:18 +0900

    test(rank): Rank.of 매핑 테스트 추가

---
+++ src/test/java/lotto/domain/RankTest.java
@@ -0,0 +1,18 @@
+package lotto.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class RankTest {
+
+    @Test
+    void of_mapping() {
+        assertThat(Rank.of(6, false)).isEqualTo(Rank.FIRST);
+        assertThat(Rank.of(5, true)).isEqualTo(Rank.SECOND);
+        assertThat(Rank.of(5, false)).isEqualTo(Rank.THIRD);
+        assertThat(Rank.of(4, false)).isEqualTo(Rank.FOURTH);
+        assertThat(Rank.of(3, false)).isEqualTo(Rank.FIFTH);
+        assertThat(Rank.of(2, false)).isEqualTo(Rank.MISS);
+    }
+}
