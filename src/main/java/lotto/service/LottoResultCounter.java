547ae716769b5111e63656ae965d89c3595d2e28
김민석 <skyfox1015@gmail.com>
2025-11-03 17:27:34 +0900

    feat(service): App에서 로또 당첨 횟수를 counting하는 것을 LottoResultCounter로 위임
    - 기존 main에서 사용하는 HashMap대신 EnumMap을 이용
    - 0으로 초기화하여서 getOrDefault의 가독성을 최소화함

--- src/main/java/lotto/service/LottoResultCounter.java
+++ src/main/java/lotto/service/LottoResultCounter.java
@@ -1,4 +1,19 @@
 package lotto.service;
 
+import lotto.domain.Rank;
+import java.util.EnumMap;
+
 public class LottoResultCounter {
+    private final EnumMap<Rank, Integer> counts = new EnumMap<>(Rank.class);
+
+    public LottoResultCounter() {
+        for (Rank r : Rank.values()) {
+            counts.put(r, 0);
+        }
+    }
+
+    public void add(Rank rank) {
+        counts.put(rank, counts.get(rank) + 1);
+    }
+
 }
