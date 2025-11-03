a95a0c6f1b22d05162898d82e12817223ceafbd3
김민석 <skyfox1015@gmail.com>
2025-11-03 18:46:53 +0900

    feat(retry): 다시 입력 받는 로직을 Money에서 제네릭으로 확대해서 구현 처리

--- src/main/java/lotto/util/Retry.java
+++ src/main/java/lotto/util/Retry.java
@@ -3,6 +3,9 @@ package lotto.util;
 import lotto.domain.Money;
 import lotto.view.InputView;
 
+import java.util.function.Function;
+import java.util.function.Supplier;
+
 public class Retry {
     private Retry() {}
 
@@ -11,6 +14,17 @@ public class Retry {
             try {
                 String input = inputView.readPurchaseAmount();
                 return Money.from(input);
+            } catch (IllegalArgumentException | IllegalStateException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    public static <T> T readParsed(Supplier<String> reader, Function<String, T> parser) {
+        while (true) {
+            try {
+                String input = reader.get();
+                return parser.apply(input);
             } catch (IllegalArgumentException | IllegalStateException e) {
                 System.out.println(e.getMessage());
             }
